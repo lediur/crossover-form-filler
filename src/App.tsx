b8e01b076e85cf16df5ee23afb9b024217352937
Derrick Liu <derrick@derrickliu.com>
2020-01-30 20:26:12 -0800

    App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PDFFiller from "./components/PDFFiller";
+import styles from "./App.module.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <PDFFiller template="ca-santa-clara" />
     </div>
   );
